c1f9d9ce094b91e4fa77e49de45af9cc21ca16e2
Code With Jeet <codewithjeet1204.com>
2024-02-16 16:00:01 +0530

    Snack Bar properties changed

--- lib/widgets/snack_bar.dart
+++ lib/widgets/snack_bar.dart
@@ -1,10 +1,19 @@
+import 'package:find_easy/utils/colors.dart';
 import 'package:flutter/material.dart';
 
 void mySnackBar(BuildContext context, String text) {
   ScaffoldMessenger.of(context).clearSnackBars();
   ScaffoldMessenger.of(context).showSnackBar(
     SnackBar(
-      content: Text(text),
+      content: Text(
+        text,
+        style: TextStyle(
+          color: primaryDark,
+        ),
+      ),
+      backgroundColor: primary,
+      dismissDirection: DismissDirection.vertical,
+      behavior: SnackBarBehavior.floating,
     ),
   );
 }
